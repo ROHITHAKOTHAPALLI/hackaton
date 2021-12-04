d6aff5da48b06221fa9731f0d8ee21e4b7679680
ROHITHA <64123943+ROHITHAKOTHAPALLI@users.noreply.github.com>
2021-12-04 20:31:44 +0530

    Add files via upload

---
+++ bookmyseat/src/main/resources/data.sql
@@ -0,0 +1,401 @@
+insert into employee(email,manager_id,password,phone_number)
+ values('ram@adp.com',10,'admin1',9876543210),
+('reshma@adp.com',5,'admin1',9999888800),
+('bheem@adp.com',5,'admin1',9456888800),
+('srikar@adp.com',5,'admin1',9456778811);
+
+insert into employee(email,manager_id,password,phone_number)
+values('prabhakar@adp.com',0,'admin1',9456778123);
+
+insert into employee(email,manager_id,password,phone_number)
+ values('ramya@adp.com',10,'admin1',9123456780),
+('lokesh@adp.com',10,'admin1',9456778199),
+('rohit@adp.com',30,'admin1',9456778188),
+('shreya@adp.com',10,'admin1',9456789760);
+
+insert into employee(email,manager_id,password,phone_number)
+values('lochan@adp.com',0,'admin1',945679999);
+
+insert into employee(email,manager_id,password,phone_number)
+ values('mani@adp.com',20,'admin1',9456778191),
+('asha@adp.com',40,'admin1',9456778333),
+('rakesh@adp.com',5,'admin1',9456789900),
+('suman@adp.com',5,'admin1',9456782340);
+
+insert into employee(email,manager_id,password,phone_number)
+values('rohitw@adp.com',0,'admin1',9886798888);
+
+
+insert into employee(email,manager_id,password,phone_number)
+ values('piyush@adp.com',20,'admin1',9456772221),
+('akhila@adp.com',40,'admin1',9452228333),
+('rose@adp.com',5,'admin1',9451119900),
+('susun@adp.com',5,'admin1',9456722240);
+ 
+ 
+  insert into employee(email,manager_id,password,phone_number)
+ values('alekhya@adp.com',0,'admin1',988670008);
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('pranay@adp.com',15,'admin1',9456778976),
+('sushmi@adp.com',15,'admin1',9452777733),
+('vandana@adp.com',10,'admin1',9451117777),
+('suhas@adp.com',15,'admin1',9456722277);
+ 
+ 
+insert into employee(email,manager_id,password,phone_number)
+ values('rohitha@adp.com',0,'admin1',7995855129);
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('yuhun@adp.com',15,'admin1',245-67789-76),
+('fanglee@adp.com',25,'admin1',245-27777-33),
+('fanglie@adp.com',30,'admin1',145-11177-77),
+('chehai@adp.com',30,'admin1',212-89878-90);
+ 
+ 
+insert into employee(email,manager_id,password,phone_number)
+ values('alen@adp.com',0,'admin1',231-78967-90);
+
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('elena@adp.com',5,'admin1','245-67777-76'),
+('riyans@adp.com',125,'admin1',8529631470),
+('damon@adp.com',20,'admin1','145-11666-77'),
+('siri@adp.com',40,'admin1',8987822290);
+ 
+ 
+insert into employee(email,manager_id,password,phone_number)
+ values('robert@adp.com',0,'admin1',231-78967-90);
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('max@adp.com',45,'admin1','245-67777-76'),
+('abdul@adp.com',25,'admin1',8529631470),
+('stefen@adp.com',30,'admin1','145-11666-77'),
+('roja@adp.com',40,'admin1',8987822290);
+ 
+ 
+insert into employee(email,manager_id,password,phone_number)
+ values('raheem@adp.com',0,'admin1',9023178967);
+
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('robin@adp.com',45,'admin1','345-67777-76'),
+('murali@adp.com',10,'admin1',8889631470),
+('vasu@adp.com',10,'admin1','145-22666-77'),
+('rama@adp.com',10,'admin1',8987812290);
+ 
+ 
+insert into employee(email,manager_id,password,phone_number)
+ values('ameen@adp.com',0,'admin1',9023179023);
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('nairobi@adp.com',45,'admin1','345-67667-76'),
+('rio@adp.com',5,'admin1','288-96314-70'),
+('berlin@adp.com',10,'admin1','045-22216-77'),
+('tatiyana@adp.com',10,'admin1','298-78122-90');
+ 
+ 
+insert into employee(email,manager_id,password,phone_number)
+ values('don@adp.com',0,'admin1','029-31790-23');
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('kim@adp.com',15,'admin1','345-66666-76'),
+('noddy@adp.com',15,'admin1','288-77777-70'),
+('oswald@adp.com',20,'admin1','045-88888-77'),
+('tom@adp.com',20,'admin1','298-99999-90'),
+('ben@adp.com',20,'admin1','123-8976-123'),
+('quin@adp.com',30,'admin1','223-8976-123'),
+('surya@adp.com',40,'admin1','8976235123'),
+('virat@adp.com',40,'admin1','8976239999'),
+('dhoni@adp.com',40,'admin1','8976278965'),
+('jonty@adp.com',20,'admin1','298-88199-90');
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('watson@adp.com',15,'admin1','345-11666-76'),
+('ashwin@adp.com',50,'admin1','8976235599'),
+('rashid@adp.com',50,'admin1','8955239999'),
+('kane@adp.com',15,'admin1','288-75557-70'),
+('warner@adp.com',25,'admin1','045-55588-77'),
+('barat@adp.com',50,'admin1','8976239555'),
+('jadeja@adp.com',50,'admin1','8976239995'),
+('rabdi@adp.com',25,'admin1','298-912349-90'),
+('abd@adp.com',25,'admin1','123-89556-123'),
+('maxwell@adp.com',25,'admin1','223-80016-123'),
+('rahul@adp.com',50,'admin1','8975321123'),
+('harman@adp.com',50,'admin1','8976239559'),
+('preeth@adp.com',50,'admin1','8955239999'),
+('imani@adp.com',50,'admin1','9576278965'),
+('afridi@adp.com',50,'admin1','188-88199-90');
+ 
+insert into employee(email,manager_id,password,phone_number)
+ values
+('elenasal@adp.com',5,'admin1','198-11666-76'),
+('stefansal@adp.com',5,'admin1','7767815599'),
+('damonsal@adp.com',15,'admin1','955239999'),
+('maxw@adp.com',15,'admin1','488-12557-70'),
+('vikky@adp.com',25,'admin1','145-51228-77'),
+('bownie@adp.com',25,'admin1','7776239555'),
+('carlione@adp.com',25,'admin1','9976239995'),
+('taylor@adp.com',30,'admin1','198-912349-90'),
+('hayley@adp.com',30,'admin1','333-12556-123'),
+('nic@adp.com',30,'admin1','223-10876-123'),
+('klaus@adp.com',30,'admin1','7675321123'),
+('kai@adp.com',30,'admin1','8116239559'),
+('hope@adp.com',40,'admin1','8955277999'),
+('liz@adp.com',40,'admin1','9576278555'),
+('joey@adp.com',40,'admin1','388-88189-90'),
+('katherin@adp.com',50,'admin1','488-88199-90');
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('pony@adp.com',45,'admin1','311-11666-76'),
+('daisy@adp.com',45,'admin1','9967815599');
+
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('abhi@adp.com',45,'admin1','111-11666-76'),
+('prem@adp.com',45,'admin1','9067815599'),
+('sruthip@adp.com',45,'admin1','885239999'),
+('lasya@adp.com',20,'admin1','223-12556-123'),
+('nandu@adp.com',20,'admin1','188-88777-90'),
+('tulasi@adp.com',45,'admin1','228-88199-00');
+ 
+ 
+ insert into employee(email,manager_id,password,phone_number)
+  values('vamshid@adp.com',0,'admin1','9223179023');
+ 
+insert into employee(email,manager_id,password,phone_number)
+  values('trump@adp.com',0,'admin1','142-23456-00'),
+('boris@adp.com',0,'admin1','142-23456-00'),
+ ('kerjrival@adp.com',0,'admin1','8923456100'),
+('modi@adp.com',0,'admin1','9923456100'),
+ ('kcr@adp.com',0,'admin1','9222345600');
+
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('rohi@adp.com',100,'admin1','911166676'),
+('anu@adp.com',100,'admin1','899815599'),
+('madhu@adp.com',100,'admin1','778923999'),
+('smaran@adp.com',100,'admin1','523-1255-223'),
+('vaishu@adp.com',100,'admin1','9088877790'),
+('venkat@adp.com',100,'admin1','8998819900');
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('herney@adp.com',101,'admin1','911555676'),
+('satwik@adp.com',101,'admin1','779815599'),
+('gopi@adp.com',101,'admin1','777723999'),
+('preetam@adp.com',101,'admin1','123-1234-223'),
+('archit@adp.com',101,'admin1','8068877790'),
+('vilas@adp.com',101,'admin1','7518819900');
+
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('sunny@adp.com',102,'admin1','978555676'),
+('sanju@adp.com',102,'admin1','779815456'),
+('chait@adp.com',102,'admin1','777727859'),
+('natraj@adp.com',102,'admin1','123-4454-223'),
+('sonal@adp.com',103,'admin1','8064654890'),
+('ramyaz@adp.com',103,'admin1','754549900');
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('vikas@adp.com',103,'admin1','963055676'),
+('anvi@adp.com',103,'admin1','745855599'),
+('honey@adp.com',103,'admin1','777565699'),
+('kartik@adp.com',104,'admin1','223-1994-223'),
+('chandu@adp.com',104,'admin1','9968877720'),
+('lazzy@adp.com',104,'admin1','321-1734-223'),
+('manga@adp.com',104,'admin1','8008877790'),
+('mouni@adp.com',104,'admin1','8118819900');
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('jahnavi@adp.com',104,'admin1','945615676'),
+('saisree@adp.com',104,'admin1','789895599'),
+('amulya@adp.com',105,'admin1','771234569'),
+('priya@adp.com',105,'admin1','223-7894-223'),
+('keerti@adp.com',105,'admin1','9912548720'),
+('sowmya@adp.com',105,'admin1','321-0000-223'),
+('mourya@adp.com',105,'admin1','8045678210'),
+('harish@adp.com',105,'admin1','8110000900');
+
+
+insert into employee(email,manager_id,password,phone_number)
+ values('prabhas@adp.com',0,'admin1','902234500');
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('mahesh@adp.com',140,'admin1','945645456'),
+('ntr@adp.com',140,'admin1','775210599'),
+('rampol@adp.com',140,'admin1','723032569'),
+('nani@adp.com',140,'admin1','221-6666-223'),
+('vijayd@adp.com',140,'admin1','9563548720'),
+('chiru@adp.com',140,'admin1','121-1010-223'),
+('adi@adp.com',140,'admin1','8777678210'),
+('charan@adp.com',140,'admin1','855500900'),
+('varuntej@adp.com',140,'admin1','9636363636');
+
+
+insert into employee(email,manager_id,password,phone_number)
+ values('kajal@adp.com',0,'admin1','111-22341-500');
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('sam@adp.com',150,'admin1','715645456'),
+('rasmika@adp.com',150,'admin1','6575210599'),
+('poojah@adp.com',150,'admin1','883032569'),
+('kritika@adp.com',150,'admin1','101-6666-223'),
+('tamanna@adp.com',150,'admin1','9040548720'),
+('tabu@adp.com',150,'admin1','040-1010-223'),
+('trisha@adp.com',150,'admin1','8237678210'),
+('kangana@adp.com',150,'admin1','90000900'),
+('hansika@adp.com',150,'admin1','9644444446');
+
+
+insert into employee(email,manager_id,password,phone_number)
+ values('deepika@adp.com',0,'admin1','111-22341-500');
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('priyanka@adp.com',160,'admin1','7156777777'),
+('ranveer@adp.com',160,'admin1','6575999999'),
+('ranbir@adp.com',160,'admin1','883000000'),
+('alia@adp.com',160,'admin1','101-6666-666'),
+('janvi@adp.com',160,'admin1','9040111111'),
+('noora@adp.com',160,'admin1','040-9999-223'),
+('karina@adp.com',160,'admin1','8222222210'),
+('shradha@adp.com',160,'admin1','9111111900'),
+('katrina@adp.com',160,'admin1','9444444444');
+
+insert into employee(email,manager_id,password,phone_number)
+ values('preet@adp.com',0,'admin1','111-11122-500');
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('harpreet@adp.com',170,'admin1','7142103677'),
+('manpreet@adp.com',170,'admin1','6510236599'),
+('sanpreet@adp.com',170,'admin1','8330011220'),
+('kanpreet@adp.com',170,'admin1','201-6120-666'),
+('janpreet@adp.com',170,'admin1','9043333311'),
+('harmank@adp.com',170,'admin1','078-9562-223'),
+('preeti@adp.com',170,'admin1','8245245210'),
+('preema@adp.com',170,'admin1','9123023023'),
+('premi@adp.com',170,'admin1','9123456944');
+
+
+
+insert into employee(email,manager_id,password,phone_number)
+ values('jain@adp.com',0,'admin1','122-89562-555');
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('kimz@adp.com',180,'admin1','001-89562-555'),
+('jack@adp.com',180,'admin1','001-892322-511'),
+('fim@adp.com',180,'admin1','221-89782-511'),
+('jill@adp.com',180,'admin1','331-87852-515'),
+('rom@adp.com',180,'admin1','441-87856-515'),
+('lam@adp.com',180,'admin1','001-82222-515'),
+('loah@adp.com',180,'admin1','021-87856-500'),
+('nova@adp.com',180,'admin1','751-87852-775'),
+('kova@adp.com',180,'admin1','021-87856-552');
+
+insert into employee(email,manager_id,password,phone_number)
+  values('heather@adp.com',0,'admin1','8975642310');
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('kaja@adp.com',190,'admin1','7112345077'),
+('barfii@adp.com',190,'admin1','6510000599'),
+('gulab@adp.com',190,'admin1','8330666221'),
+('jamun@adp.com',190,'admin1','201-6120-012'),
+('rasmal@adp.com',0,'admin1','9043101011'),
+('sweety@adp.com',195,'admin1','078-9412-223'),
+('halw@adp.com',195,'admin1','8210325210'),
+('kesar@adp.com',195,'admin1','9123010233'),
+('keer@adp.com',195,'admin1','7125010233'),
+('ponga@adp.com',195,'admin1','9111144514');
+
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('lily@adp.com',100,'admin1','7896547077'),
+('jasmine@adp.com',100,'admin1','6520110599'),
+('marie@adp.com',100,'admin1','8330666221'),
+('tulip@adp.com',100,'admin1','201-0022-012'),
+('gum@adp.com',5,'admin1','900101011'),
+('fevi@adp.com',5,'admin1','078-9222-223'),
+('jel@adp.com',5,'admin1','210-32521-110'),
+('glue@adp.com',5,'admin1','123-01023-883'),
+('stik@adp.com',5,'admin1','12-50102-133');
+
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('ausu@adp.com',140,'admin1','7896540017'),
+('hpee@adp.com',140,'admin1','6520110444'),
+('acer@adp.com',140,'admin1','8330666221'),
+('samsu@adp.com',170,'admin1','781-0077-012'),
+('noki@adp.com',170,'admin1','900178999'),
+('realh@adp.com',170,'admin1','788-7822-223'),
+('redk@adp.com',170,'admin1','900-38971-110'),
+('vivoi@adp.com',190,'admin1','023-78923-883'),
+('appu@adp.com',195,'admin1','112-57802-133');
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('koamk@adp.com',30,'admin1','9555540017'),
+('hyde@adp.com',30,'admin1','7894510444'),
+('suro@adp.com',30,'admin1','9633696221'),
+('zuno@adp.com',30,'admin1','781-7577-012'),
+('loki@adp.com',30,'admin1','702178999'),
+('saki@adp.com',101,'admin1','165-4561-223'),
+('flika@adp.com',101,'admin1','100-23654-110'),
+('amazi@adp.com',102,'admin1','223-77853-883'),
+('ajoo@adp.com',102,'admin1','132-23501-133');
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('sarat@adp.com',25,'admin1','7632550017'),
+('amb@adp.com',25,'admin1','7888522252'),
+('forum@adp.com',25,'admin1','9666666221'),
+('cinep@adp.com',150,'admin1','711-1237-012'),
+('cineg@adp.com',150,'admin1','932178999'),
+('galxy@adp.com',150,'admin1','125-6301-223'),
+('ramba@adp.com',150,'admin1','106-23354-110'),
+('vishu@adp.com',150,'admin1','933-77883-883'),
+('prvati@adp.com',25,'admin1','332-26301-133');
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('bella@adp.com',160,'admin1','7556550017'),
+('jack@adp.com',160,'admin1','125-55225-552'),
+('edward@adp.com',160,'admin1','9666666221');
+
+
+insert into employee(email,manager_id,password,phone_number)
+  values('bali@adp.com',0,'admin1','8975642310');
+
+insert into employee(email,manager_id,password,phone_number)
+ values
+('balla@adp.com',240,'admin1','256-55001-997'),
+('bahu@adp.com',240,'admin1','115-53325-552'),
+('deva@adp.com',240,'admin1','9777756221'),
+('sivag@adp.com',240,'admin1','011-1237-012'),
+('kalk@adp.com',240,'admin1','989562999'),
+('bejal@adp.com',240,'admin1','111-6301-223'),
+('asura@adp.com',240,'admin1','366-23354-110'),
+('jekana@adp.com',240,'admin1','455-77883-883'),
+('amara@adp.com',240,'admin1','112-77883-883'),
+('avanti@adp.com',240,'admin1','112-26301-133');
